d52f99ea0d00e32d1ebe9d1d07a5ff18f08bed3d
Shreedhar <urawane03@gmail.com>
2023-07-30 16:13:23 +0530

    add student report screen logic

--- client/src/Components/ReportBox.jsx
+++ client/src/Components/ReportBox.jsx
@@ -5,7 +5,7 @@ const ReportBox = ({subject,total,attended}) => {
     <div className='bg-[var(--primary)] rounded-lg w-24'>
       <p className='text-white text-center text-xs pt-1 pb-[2px]'>{subject}</p>
       <div className="rounded-md bg-white text-black p-2 ml-[2px] mr-[2.5px] mb-[2px]">
-        <p className='text-2xl'>{(attended/total*100).toFixed(0)}%</p>
+        <p className='text-xl font-semibold'>{(attended/total*100).toFixed(1)}%</p>
         <p className='text-xs'>{attended}/{total}</p>
       </div>
     </div>
--- client/src/Components/Student_Info.jsx
+++ client/src/Components/Student_Info.jsx
@@ -3,48 +3,76 @@ import ReportBox from './ReportBox'
 import { useNavigate } from 'react-router-dom'
 
 const Student_Info = () => {
-    const navigate=useNavigate()
+    const navigate = useNavigate()
     const [record, setRecord] = useState({
         roll: window.location.href.split("/").slice(-1)[0],
         name: "Bade Akshay Bhagwan",
-        totalLectures: 18,
-        totalLecturesAttended: 13,
-        totalPracticals: 18,
-        totalPracticalsAttended: 15,
+        totalLectures: 100,
+        totalLecturesAttended: 75,
+        totalPracticals: 50,
+        totalPracticalsAttended: 50,
         theoryDistribution:
             [
-                { subject: "DBMS", total: 18, attended: 17 },
-                { subject: "SPOS", total: 18, attended: 4 },
-                { subject: "CN", total: 18, attended: 10 },
-                { subject: "EL1", total: 18, attended: 8 },
-                { subject: "TOC", total: 18, attended: 18 },
+                { subject: "DBMS", total: 24, attended: 18 },
+                { subject: "SPOS", total: 24, attended: 22 },
+                { subject: "CN", total: 24, attended: 19 },
+                { subject: "EL1", total: 24, attended: 18 },
+                { subject: "TOC", total: 24, attended: 15 },
+            ],
+        practicalDistribution:
+            [
+                { subject: "DBMSL", total: 12, attended: 7 },
+                { subject: "LP1", total: 12, attended: 12 },
+                { subject: "CNSL", total: 14, attended: 12 },
+
             ],
     })
+    const getPercent = (totalLectures, totalLecturesAttended, totalPracticals, totalPracticalsAttended) => {
+        const total = (totalLecturesAttended + totalPracticalsAttended) / (totalLectures + totalPracticals) * 100
+        return (
+            total === 100 ? total : total.toFixed(1)
+        )
+    }
+    const { totalLectures, totalLecturesAttended, totalPracticals, totalPracticalsAttended } = record
     return (
         <div className='flex flex-col bg-slate-10 px-6 my-6'>
-            <button className='self-start text-3xl text-gray-700' onClick={()=>navigate('/search')}>&larr;</button>
+            <button className='self-start text-3xl text-gray-700' onClick={() => navigate('/search')}>&larr;</button>
             <h1 className='text-xl font-semibold text-[var(--primary)] mt-4'>{record.roll}</h1>
             <h1 className='abel text-2xl'>{record.name}</h1>
             <div className='flex justify-between mt-4 border-2 border-gray-400 rounded-lg p-3'>
                 <div className='border-r-2 w-1/2 pr-3 border-gray-400'>
                     <p className='text-sm'>OVERALL</p>
-                    <h1 className='text-6xl font-extrabold text-[#008000]'>78%</h1>
-                    <p className=''>24/38</p>
+                    <h1 className='text-4xl font-extrabold text-[#008000] mt-2'>
+                        {getPercent(totalLectures, totalLecturesAttended, totalPracticals, totalPracticalsAttended)}%
+                    </h1>
+                    <p className='mt-1'>{totalLecturesAttended + totalPracticalsAttended}/{totalLectures + totalPracticals}</p>
                 </div>
                 <div className='w-1/2 pl-3'>
                     <h1 className='text-sm'>THEORY</h1>
-                    <p><span className='text-3xl'>84%</span><span className='text-lg text-gray-600 ml-1'>12/18</span></p>
+                    <p><span className='text-lg'>{(totalLecturesAttended / totalLectures * 100).toFixed(1)}%</span><span className='text-xs text-gray-600 ml-2'>{totalLecturesAttended}/{totalLectures}</span></p>
                     <h1 className='mt-2 text-sm'>LABS</h1>
-                    <p><span className='text-3xl'>84%</span><span className='text-lg text-gray-600 ml-1'>12/18</span></p>
+                    <p><span className='text-lg'>{(totalPracticalsAttended / totalPracticals * 100).toFixed(1)}%</span><span className='text-xs text-gray-600 ml-2'>{totalPracticalsAttended}/{totalPracticals}</span></p>
                 </div>
             </div>
-            <div className='mt-4'>
+            <div className='mt-8'>
                 <p className='uppercase text-sm text-gray-700'>theory distribution</p>
                 <div className='flex gap-3 flex-wrap mt-4'>
                     {
                         record.theoryDistribution.map((e, key) => {
                             return (
-                                <ReportBox subject={e.subject} attended={e.attended} total={e.total} />
+                                <ReportBox key={key} subject={e.subject} attended={e.attended} total={e.total} />
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            <div className='mt-8'>
+                <p className='uppercase text-sm text-gray-700'>practical distribution</p>
+                <div className='flex gap-3 flex-wrap mt-4'>
+                    {
+                        record.practicalDistribution.map((e, key) => {
+                            return (
+                                <ReportBox key={key} subject={e.subject} attended={e.attended} total={e.total} />
                             )
                         })
                     }
