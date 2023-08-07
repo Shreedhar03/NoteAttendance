b63a190b9b3027de52b991d605c506ed8e3af35e
Shreedhar <urawane03@gmail.com>
2023-08-07 17:53:33 +0530

    add 'specific user' access, remove firebase.js

--- client/src/Components/Choices.jsx
+++ client/src/Components/Choices.jsx
@@ -2,13 +2,11 @@ import React, { useContext, useEffect, useState } from 'react'
 import Radio from './Radio'
 import { Link, useNavigate } from 'react-router-dom'
 import { subjects } from '../../subjects.js'
-import Dialog from './Dialog'
 import { AppContext } from '../App'
 
 const Choices = () => {
     const { user, signOutWithGoogle } = useContext(AppContext)
     const goto = useNavigate()
-    const [dialog, setDialog] = useState(false)
     const [formValues, setFormValues] = useState({
         year: "SE",
         division: "A",
@@ -28,11 +26,6 @@ const Choices = () => {
         e.preventDefault()
         goto('/attendance')
         console.log(formValues)
-        // console.log("okk")
-        // console.log(subjects)
-    }
-    const handleCancel = () => {
-        setDialog(false)
     }
     useEffect(() => {
         setTheorySubjects(subjects[formValues.year].theory)
@@ -49,7 +42,7 @@ const Choices = () => {
                 <button className='text-sm bg-red-700 p-1 px-2 text-white rounded-lg' onClick={signOutWithGoogle}>Logout</button>
             </nav>
             <form className='flex flex-col mt-8 gap-8 text-xl px-6 relative' onSubmit={handleSubmit}>
-                <div className={` ${dialog && 'opacity-10'}`}>
+                <div className={` 'opacity-10'}`}>
                     <h1 className='text-2xl mb-2'>Year</h1>
                     <div className="flex gap-3">
                         <Radio label="SE" value={formValues.year} handleChange={handleChange} name="year" sm={true} />
@@ -58,7 +51,7 @@ const Choices = () => {
                     </div>
                 </div>
 
-                <div className={`${dialog && 'opacity-10'}`}>
+                <div className={`'opacity-10'}`}>
                     <h1 className='text-2xl mb-2'>Division</h1>
                     <div className="flex gap-3">
                         <Radio handleChange={handleChange} value={formValues.division} label="A" name="division" sm={true} />
@@ -68,12 +61,12 @@ const Choices = () => {
                     </div>
                 </div>
 
-                <div className={`flex ${dialog && 'opacity-10'}`}>
+                <div className={`flex 'opacity-10'}`}>
                     <Radio handleChange={handleChange} value={formValues.session} label="Theory" name="session" sm={false} border_l={true} />
                     <Radio handleChange={handleChange} value={formValues.session} label="Practical" name="session" sm={false} border_r={true} />
                 </div>
 
-                <div className={` ${dialog && 'opacity-10'}`}>
+                <div className={` 'opacity-10'}`}>
                     {
                         formValues.session === "Practical" &&
                         <select name="labSubject" id="labSubject" onChange={handleChange} value={formValues.labSubject} required className='p-3 rounded-lg focus:outline-none bg-inherit border border-[var(--primary)]'>
@@ -100,14 +93,14 @@ const Choices = () => {
                     }
                 </div>
 
-                <div className={`w-full mt-4 ${dialog && 'opacity-10'}`}>
+                <div className={`w-full mt-4 'opacity-10'}`}>
                     <Link to={'/search'} className='block my-3 text-lg underline'>Check Student Report</Link>
                     <input type="submit" value="Proceed" className='bg-[var(--primary)] w-full p-3 rounded-lg text-white' />
                 </div>
 
-                <Dialog handleCancel={handleCancel} dialog={dialog} handleSubmit={handleSubmit} message="Entry Already exists for this date. Do You want to continue?" />
 
             </form>
+                {/* <Dialog handleCancel={handleCancel} dialog={dialog} handleSubmit={handleSubmit} message="Entry Already exists for this date. Do You want to continue?" /> */}
         </>
 
     )
